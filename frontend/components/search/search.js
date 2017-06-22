eb892e1968e5780191a74b9d7474f783f0b8e356
davidnoah <david.t.noah@gmail.com>
2017-06-22 00:58:07 -0700

    fix search placeholder

--- frontend/components/search/search.js
+++ frontend/components/search/search.js
@@ -5,7 +5,7 @@ const Search = ({ handleChange, searchPhotos, keyPress }) => {
   return (
     <div className={'searchbarContainer'}>
       <i className={"fa fa-picture-o"} style={{ ariaHidden: "true", fontSize: '36px', marginRight: '10px'}}></i>
-      <input className={'searchInput'} onChange={handleChange} onKeyPress={keyPress} id={'query'} placeholder={'Type in a dope word...'}/>
+      <input className={'searchInput'} onChange={handleChange} onKeyPress={keyPress} id={'query'} placeholder={'Type in a word...'}/>
       <button className={'searchButton'} onClick={searchPhotos}>Search</button>
     </div>
   );
